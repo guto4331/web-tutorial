6282d17687c15762773d854a412dc56fbee062a1
guto4331 <guto4331@gmail.com>
2024-02-21 19:18:00 +0900

    feat: ユーザー名入力を求めるように変更
    画面最下部のボタンでユーザー名を変更することも可能です。h1 要素を変更して挨拶するようになりました。

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,27 @@ myImage.onclick = () => {
         myImage.setAttribute("src", "images/buncho.jpg");
     }
 };
+
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    const myName = prompt("あなたの名前を入力してください。");
+    if(!myName){
+        setUserName();
+    }else{
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `${myName} さん、Mozilla はかっこいいよ。`;
+    }
+}
+
+if(!localStorage.getItem("name")){
+    setUserName();
+}else{
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `${storedName} さん、Mozilla はかっこいいよ。`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+}
